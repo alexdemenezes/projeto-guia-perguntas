99395327b6ac0b300121b42a6121c852bf413351
alexdemenezes <alexmenezes.business@outlook.com>
2022-05-31 08:42:55 -0300

    configuracao do form para enviar os dados do formulario para o endpoint

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser')
 
 const PORT = 3000;
 
 
 // estou dizendo para o Express usar o EJS como View engine
 app.set('view engine', 'ejs')
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -17,4 +19,13 @@ app.get('/', (req, res) => {
 app.get('/question', (req, res) => {
   res.render('question');
 });
+
+app.post('/savequestion', (req, res) => {
+  const { title } = req.body;
+  const { description } = req.body
+  res.send(`titulo: ${title}, descricao: ${description}`);
+});
+
+
+
 app.listen(PORT, () => console.log(`servidor está escutando na porta: ${PORT}`))
